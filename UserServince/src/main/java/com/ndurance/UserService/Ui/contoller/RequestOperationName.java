f70148987823834581ef074d69be9d35641dc2e3
Manura Sanjula <w.m.manurasanjula2003@gmail.com>
2024-08-26 10:04:18 +0530

    26/08/2024 commit : fix: resolve problems with Java package structure

--- UserServince/src/main/java/com/ndurance/UserService/Ui/contoller/RequestOperationName.java
+++
@@ -1,8 +0,0 @@
-package com.ndurance.UserService.Ui.contoller;
-
-public enum RequestOperationName {
-	DELETE,
-	VERIFY_EMAIL,
-	REQUEST_PASSWORD_RESET,
-	PASSWORD_RESET
-}
--- UserServince/src/main/java/com/ndurance/UserService/Ui/model/response/RequestOperationStatus.java
+++
@@ -1,5 +0,0 @@
-package com.ndurance.UserService.Ui.model.response;
-
-public enum RequestOperationStatus {
-	ERROR, SUCCESS
-}
