f454e664af0f7213896a14b7cb19113c8f40563f
Hansana <hansanathimeth@gmail.com>
2024-08-28 13:46:02 +0530

    Added temp shop data and updated items page

--- ndurance/src/app/page.js
+++ ndurance/src/app/page.js
@@ -1,14 +1,11 @@
 "use client";
 
 import Navigation from "./navigation";
-import {Button} from "flowbite-react";
 
 export default function Home() {
     return (
         <div>
             <Navigation/>
-            <Button>Add to Cart</Button>
-            <Button>Remove from Cart</Button>
         </div>
     );
 }
