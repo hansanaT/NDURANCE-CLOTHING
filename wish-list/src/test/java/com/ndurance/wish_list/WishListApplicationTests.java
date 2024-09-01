109df3e18b74e9f2e5778685b7a9d45ba75cb863
Manura Sanjula <w.m.manurasanjula2003@gmail.com>
2024-09-01 23:18:28 +0530

    01/09/2024 2nd Commit

---
+++ wish-list/src/test/java/com/ndurance/wish_list/WishListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndurance.wish_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
